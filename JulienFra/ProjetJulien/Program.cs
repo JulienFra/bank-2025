92c2b810cf5ac7e61eb0453415281d13e4ff359c
JulienFra <julien20082002@hotmail.com>
2025-10-11 12:47:17 +0200

    creating class

--- JulienFra/ProjetJulien/Program.cs
+++ JulienFra/ProjetJulien/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Person
+{
+    public string FirstName;
+    public string lastName;
+    public DateTime BirthDate;
+
+}
+
+public class currentAccount
+{
+    public string number;
+    public double balance { get; }
+    public double creditLine;
+    public Person owner;
+
+    public void Withdraw(double amount)
+    {
+
+    }
+    public void Deposit(double amount)
+    {
+
+    }
+}
+
+public class Bank
+{
+    public Dictionary<string, currentAccount> Account { get; }
+    public string name;
+}
